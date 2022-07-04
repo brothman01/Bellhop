80bf587068dd06845ba5cfdd53618720ae32e745
Ben <Ben@BenRothman.org>
2022-07-04 12:56:28 -0400

    added uglify to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,16 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-  
+      uglify: {
+        options: {
+          mangle: false
+        },
+        my_target: {
+          files: {
+            'assets/js/input.min.js': ['assets/js/input.js']
+          }
+        }
+      },
       watch: {
         sass: {
           files: 'assets/css/*.scss',
@@ -30,10 +39,13 @@ module.exports = function(grunt) {
   
     // Default task
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('css', ['sass', 'cssmin']);
+    grunt.registerTask('css', 'sass');
   
     // Load up tasks
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-jslint');
+    
   
   };
