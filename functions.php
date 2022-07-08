c566fae9ae1cdef3cbcb8eaf7f818176f8c0739d
Ben <Ben@BenRothman.org>
2022-07-08 10:57:18 -0400

    fixed the php file to be OOP

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  * Description:       Just a simple WordPress ConciergeWP is a lightweight responsive bellhop button that allows for any visitors to your site to contact the front desk easily and in several ways.
  * Requires at least: 5.9
  * Requires PHP:      7.0
- * Version:           1.0.0
+ * Version:           0.1
  * Author:            Ben Rothman
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -12,16 +12,44 @@
  *
  * @package           conciergewp
  */
-
-
- add_action( 'wp_footer', 'footer_hook' );
- 
- function footer_hook() {
- 	echo '<div id="render_here" style="position: fixed; top: 85%; left: 90%;"></div>';
- }
- 
-function enqueue_script() {
-	wp_enqueue_style( 'style-name', plugin_dir_url( __FILE__ ) . 'build/style-index.css' );
-	wp_enqueue_script( 'test', plugin_dir_url( __FILE__ ) . '/build/index.js', array( 'wp-element' ), '0.1.0', true );
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'unauthorized' );
 }
-add_action( 'wp_enqueue_scripts', 'enqueue_script' );
+
+ class ConciergeWP {
+
+	/**
+	 * ConciergeWP class constructor
+	 *
+	 * @since 0.1
+	 */
+	public function __construct() {
+
+		add_action( 'wp_footer', array( $this,'cwp_footer_hook' ) );
+
+		add_action( 'wp_enqueue_scripts', array( $this,'cwp_enqueue_script' ) );
+	}
+
+	/**
+	 * Echos the root element for React to hook onto
+	 *
+	 * @since 0.1
+	 */
+	public function cwp_footer_hook() {
+		echo '<div id="render_here" style="position: fixed; top: 85%; left: 90%;"></div>';
+	}
+
+	/**
+	 * Enqueue the style and react used by this plugin.
+	 *
+	 * @since 0.1
+	 */
+	function cwp_enqueue_script() {
+		wp_enqueue_style( 'style', plugin_dir_url( __FILE__ ) . 'build/style-index.css' );
+
+		wp_enqueue_script( 'react-code', plugin_dir_url( __FILE__ ) . '/build/index.js', array( 'wp-element' ), '0.1.0', true );
+	}
+
+ }
+
+ new ConciergeWP();
